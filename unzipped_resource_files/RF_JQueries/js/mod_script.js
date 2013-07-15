403d13fe4324c963ac08ec2a94963194a0566c36
Jonathan Freese <jfreese@appirio.com>
2013-07-15 07:17:57 -0400

    2013-07-14 update RF_JQueries/js/mod_script.js to disable modified header desc click-wrapper - did not work as yet

--- unzipped_resource_files/RF_JQueries/js/mod_script.js
+++ unzipped_resource_files/RF_JQueries/js/mod_script.js
@@ -7,42 +7,53 @@
 //
 //-----------------------------------------------------------------------------------------------------------------
 
-jQuery(".header .desc-wrapper").click(function() {
-    var w = jQuery(this);
-    var d = jQuery(".dropdown", w);
-    var s = jQuery("span.name", w);
-    w.toggleClass("click-wrapper");
-    if(w.hasClass("click-wrapper")) {
-        d.css("width", s.width()+10);
-        s.animate({
-            height: 30  //this is the only change, needed because other profile dropdown options are hidden
-        }, 200);
-        d.show();
-    }
-    else {
-        d.hide();
-        s.animate({
-            height: 15
-        }, 200);
-    }
-});
+var $j = jQuery.noConflict();
 
-jQuery(".breadcrumb .site-nav-wrapper").click(function() {
-    var w = jQuery(this);
-    var d = jQuery(".dropdown", w);
-    var s = jQuery("span.nav", w);
-    w.toggleClass("click-wrapper");
-    if(w.hasClass("click-wrapper")) {
-        d.css("width", s.width()+10);
-        s.animate({
-            height: 150
-        }, 200);
-        d.show();
-    }
-    else {
-        d.hide();
-        s.animate({
-            height: 15
-        }, 200);
-    }
+$j(document).ready(function() {
+
+   //Modify the dropdown height from 150 to 45, because other profile dropdown options are hidden
+   //Tried to use this to overwrite the same function in script.js, but they appear vto
+   //cancel each other out.  Clicking on the profile dropdown does nothing.
+   //No console errors, but commenting this out fvor now.
+//    $j(".header .desc-wrapper").click(function() {
+//        var w = $j(this);
+//        var d = $j(".dropdown", w);
+//        var s = $j("span.name", w);
+//        w.toggleClass("click-wrapper");
+//        if(w.hasClass("click-wrapper")) {
+//            d.css("width", s.width()+10);
+//            s.animate({
+//                height: 45
+//            }, 200);
+//            d.show();
+//        }
+//        else {
+//            d.hide();
+//            s.animate({
+//                height: 15
+//            }, 200);
+//        }
+//    });
+
+
+    //New jQuery for the breadcrumb
+    $j(".breadcrumb .site-nav-wrapper").click(function() {
+        var w = $j(this);
+        var d = $j(".dropdown", w);
+        var s = $j("span.nav", w);
+        w.toggleClass("click-wrapper");
+        if(w.hasClass("click-wrapper")) {
+            d.css("width", s.width()+10);
+            s.animate({
+                height: 150
+            }, 200);
+            d.show();
+        }
+        else {
+            d.hide();
+            s.animate({
+                height: 15
+            }, 200);
+        }
+    });
 });
